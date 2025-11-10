324281a5277e3e9948a4bb4d176bcaa0de942f4f
Tingyicty <tingyi.chen@mail.mcgill.ca>
2025-11-10 13:01:50 -0500

    adding motor code

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -59,6 +59,14 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define IN1_Pin GPIO_PIN_0
+#define IN1_GPIO_Port GPIOA
+#define IN2_Pin GPIO_PIN_1
+#define IN2_GPIO_Port GPIOA
+#define IN3_Pin GPIO_PIN_2
+#define IN3_GPIO_Port GPIOA
+#define IN4_Pin GPIO_PIN_3
+#define IN4_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
